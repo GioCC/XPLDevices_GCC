6cdf2bdcec1f12f6bfde40a0c700898740604934
GioCC <g.crocic@gmail.com>
2023-04-04 15:40:14 +0200

    Config and dummy files to allow compilation under Platform.io

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <XPLDevices.h>
+
+// Dummy main.cpp, used to allow project compilation under Platform.io
+
+void setup() 
+{ 
+}
+
+void loop() 
+{
+};
+
+// int main(void)
+// {
+//     setup();
+//     while(1) loop();
+// };
