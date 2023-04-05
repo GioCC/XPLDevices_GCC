590ef7f226cf4747e43aaa76a9027f6f2e7db3a6
Giorgio Croci Candiani <g,crocic@gmail.com>
2023-04-05 22:09:02 +0200

    First adaptation of Encoder.h
    Removed button from encoder (handle button separately, or derive new EncoderPush class inheriting Button)
    Removed functions to assign command through string rather than cmd code
    NB Code still present but commented out

--- include/Encoder.h
+++ include/Encoder.h
@@ -3,116 +3,139 @@
 #include <Arduino.h>
 #include <DigitalIn.h>
 
-enum EncCmd_t
-{
-  encCmdUp,
-  encCmdDown,
-  encCmdPush
-};
-
-enum EncPulse_t
-{
-  enc1Pulse = 1,
-  enc2Pulse = 2,
-  enc4Pulse = 4
-};
-
-/// @brief Class for rotary encoders with optional push functionality. The number of counts per mechanical notch can be 
+/// @brief Class for simple rotary encoders.
+/// Without button (handled separately). 
+/// The number of counts per mechanical notch can be
 /// configured for the triggering of up/down events.
 class Encoder
 {
 public:
-  /// @brief Constructor. Sets connected pins and number of counts per notch.
-  /// @param nExp expander number (from initialization order)
-  /// @param pin1 pin for Encoder A track
-  /// @param pin2 pin for Encoder B track
-  /// @param pin3 pin for Encoder push function (NOT_USED if not connected)
-  /// @param pulses Number of counts per mechanical notch
-  Encoder(uint8_t nExp, uint8_t pin1, uint8_t pin2, uint8_t pin3, EncPulse_t pulses);
 
-  /// @brief Constructor. Sets connected pins and number of counts per notch.
-  /// @param pin1 pin for Encoder A track
-  /// @param pin2 pin for Encoder B track
-  /// @param pin3 pin for Encoder push function (NOT_USED if not connected)
-  /// @param pulses Number of counts per mechanical notch
-  Encoder(uint8_t pin1, uint8_t pin2, uint8_t pin3, EncPulse_t pulses) : Encoder(NOT_USED, pin1, pin2, pin3, pulses) {}
+    enum EncCmd_t {
+        encCmdUp,
+        encCmdDown,
+        encCmdFastUp,
+        encCmdFastDown,
+        // encCmdPush
+    };
 
-  /// @brief Handle realtime. Read input and evaluate any transitions.
-  void update();
+    enum EncPulse_t {
+        enc1Pulse = 1,
+        enc2Pulse = 2,
+        enc4Pulse = 4
+    };
 
-  /// @brief Handle realtime and process XPLDirect commands.
-  void process()   { update(); trigger(); };
+    using callback = void(*)(uint8_t);
 
-  /// @brief Read current Encoder count.
-  /// @return Remaining Encoder count.
-  int16_t pos()     { return _count; };
+    /// @brief Constructor. Sets connected pins and number of counts per notch.
+    /// @param nExp expander number (from initialization order)
+    /// @param pin1 pin for Encoder A track
+    /// @param pin2 pin for Encoder B track
+    // /// @param pin3 pin for Encoder push function (NOT_USED if not connected)
+    /// @param pulses Number of counts per mechanical notch
+    // Encoder(uint8_t nExp, uint8_t pin1, uint8_t pin2, uint8_t pin3, EncPulse_t pulses);
+    Encoder(uint8_t nExp, uint8_t pin1, uint8_t pin2, EncPulse_t pulses);
 
-  /// @brief Evaluate Encoder up one notch (positive turn) and consume event
-  /// @return true: up event available and transition reset.
-  bool up()         { return _count >= _pulses ? (_count -= _pulses, true) : false; };
-  
-  /// @brief Evaluate Encoder up down notch (negative turn) and consume event
-  /// @return true: up event available and transition reset.
-  bool down()       { return _count <= -_pulses ? (_count += _pulses, true) : false; };
-  
-  /// @brief Evaluate and reset transition if Encoder pressed down
-  /// @return true: Button was pressed. Transition detected and reset.
-  bool pressed()    { return _transition == transPressed  ? (_transition = transNone, true) : false; };
+    /// @brief Constructor. Sets connected pins and number of counts per notch.
+    /// @param pin1 pin for Encoder A track
+    /// @param pin2 pin for Encoder B track
+    // /// @param pin3 pin for Encoder push function (NOT_USED if not connected)
+    /// @param pulses Number of counts per mechanical notch
+    // Encoder(uint8_t pin1, uint8_t pin2, uint8_t pin3, EncPulse_t pulses)
+    //     : Encoder(NOT_USED, pin1, pin2, pin3, pulses) {}
+    Encoder(uint8_t pin1, uint8_t pin2, EncPulse_t pulses)
+        : Encoder(NOT_USED, pin1, pin2, pulses) {}
 
-  /// @brief Evaluate and reset transition if Encoder released
-  /// @return true: Button was released. Transition detected and reset.
-  bool released()   { return _transition == transReleased ? (_transition = transNone, true) : false; };
+    /// @brief Set callback functions for encoder events.
+    ///        Callback functions have prototype: void(*)(uint8_t) 
+    /// @param cb_onUp   Callback for "up" event
+    /// @param cb_onDown Callback for "down" event
+    // /// @param cb_onFastUp   Callback for fast "up" event
+    // /// @param cb_onFastDown Callback for fast "down" event
+    void setCallback(callback cb_onUp, callback cb_onDown
+        // , callback cb_onFastUp, callback cb_onFastDown
+    )
+        { onUp = cb_onUp; onDown = cb_onDown; 
+            // onFastUp = cb_onFastUp; onFastDown = cb_onFastDown;
+        }
 
-  /// @brief Evaluate status of Encoder push function
-  /// @return true: Button is currently held down  
-  bool engaged()    { return _state > 0; };
+    /// @brief Handle realtime. Read input and evaluate any transitions.
+    void update();
 
-  /// @brief Set XPLDirect commands for Encoder events
-  /// @param cmdUp Command handle for positive turn as returned by XP.registerCommand()
-  /// @param cmdDown Command handle for negative turn as returned by XP.registerCommand()
-  /// @param cmdPush Command handle for push as returned by XP.registerCommand()
-  void setCommand(int cmdUp, int cmdDown, int cmdPush);
+    /// @brief Handle realtime and process XPLDirect commands.
+    void process() { update(); trigger(); };
 
-  /// @brief Set XPLDirect commands for Encoder events
-  /// @param cmdNameUp Command for positive turn
-  /// @param cmdNameDown Command for negative turn
-  /// @param cmdNamePush Command for push
-  void setCommand(XPString_t *cmdNameUp, XPString_t *cmdNameDown, XPString_t *cmdNamePush);
+    /// @brief Read current Encoder count.
+    /// @return Remaining Encoder count.
+    int16_t pos() { return _count; };
 
-  /// @brief Set XPLDirect commands for Encoder events without push function
-  /// @param cmdUp Command handle for positive turn as returned by XP.registerCommand()
-  /// @param cmdDown Command handle for negative turn as returned by XP.registerCommand()
-  void setCommand(int cmdUp, int cmdDown);
+    /// @brief Evaluate Encoder up one notch (positive turn) and consume event
+    /// @return true: up event available and transition reset.
+    bool    up() { return _count >= _pulses ? (_count -= _pulses, true) : false; };
 
-  /// @brief Set XPLDirect commands for Encoder events
-  /// @param cmdNameUp Command for positive turn
-  /// @param cmdNameDown Command for negative turn
-  void setCommand(XPString_t *cmdNameUp, XPString_t *cmdNameDown);
+    /// @brief Evaluate Encoder up down notch (negative turn) and consume event
+    /// @return true: up event available and transition reset.
+    bool    down() { return _count <= -_pulses ? (_count += _pulses, true) : false; };
 
-  /// @brief Get XPLDirect command assiciated with the selected event
-  /// @param cmd Event to read out (encCmdUp, encCmdDown, encCmdPush)
-  /// @return Handle of the command, -1 = no command
-  int getCommand(EncCmd_t cmd);
+    // /// @brief Evaluate and reset transition if Encoder pressed down
+    // /// @return true: Button was pressed. Transition detected and reset.
+    // bool    pressed() { return _transition == transPressed ? (_transition = transNone, true) : false; };
 
-  /// @brief Check for Encoder events and process XPLDirect commands as appropriate
-  void trigger();
+    // /// @brief Evaluate and reset transition if Encoder released
+    // /// @return true: Button was released. Transition detected and reset.
+    // bool    released() { return _transition == transReleased ? (_transition = transNone, true) : false; };
+
+    // /// @brief Evaluate status of Encoder push function
+    // /// @return true: Button is currently held down
+    // bool    engaged() { return _state > 0; };
+
+    // /// @brief Set XPLDirect commands for Encoder events
+    // /// @param cmdUp Command handle for positive turn as returned by XP.registerCommand()
+    // /// @param cmdDown Command handle for negative turn as returned by XP.registerCommand()
+    // /// @param cmdPush Command handle for push as returned by XP.registerCommand()
+    // // void    setCommand(int cmdUp, int cmdDown, int cmdPush);
+    // void    setCommand(int cmdUp, int cmdDown);
+
+    // /// @brief Set XPLDirect commands for Encoder events
+    // /// @param cmdNameUp Command for positive turn
+    // /// @param cmdNameDown Command for negative turn
+    // /// @param cmdNamePush Command for push
+    // void    setCommand(XPString_t *cmdNameUp, XPString_t *cmdNameDown, XPString_t *cmdNamePush);
+
+    /// @brief Set XPLDirect commands for Encoder events without push function
+    /// @param cmdUp Command handle for positive turn as returned by XP.registerCommand()
+    /// @param cmdDown Command handle for negative turn as returned by XP.registerCommand()
+    void    setCommand(int cmdUp, int cmdDown);
+
+    // /// @brief Set XPLDirect commands for Encoder events
+    // /// @param cmdNameUp Command for positive turn
+    // /// @param cmdNameDown Command for negative turn
+    // void    setCommand(XPString_t *cmdNameUp, XPString_t *cmdNameDown);
+
+    /// @brief Get XPLDirect command assiciated with the selected event
+    /// @param cmd Event to read out (encCmdUp, encCmdDown, encCmdPush)
+    /// @return Handle of the command, -1 = no command
+    int     getCommand(EncCmd_t cmd);
+
+    /// @brief Check for Encoder events and process XPLDirect commands as appropriate
+    void    trigger();
+
 private:
-  enum
-  {
-    transNone,
-    transPressed,
-    transReleased
-  };
-  uint8_t _nExp;
-  uint8_t _pin1, _pin2, _pin3;
-  int8_t _count;
-  uint8_t _pulses;
-  uint8_t _state;
-  uint8_t _debounce;
-  uint8_t _transition;
-  int _cmdUp;
-  int _cmdDown;
-  int _cmdPush;
+    static callback onUp;
+    static callback onDown;
+    // static callback onFastUp;
+    // static callback onFastDown;
+
+    uint8_t _nExp;
+    uint8_t _pin1, _pin2;
+    int8_t  _count;
+    uint8_t _pulses;
+    uint8_t _state;
+    // uint8_t _debounce;
+    // uint8_t _transition;
+    int     _cmdUp;
+    int     _cmdDown;
+    // int     _cmdPush;
 };
 
 #endif
--- src/Encoder.cpp
+++ src/Encoder.cpp
@@ -2,148 +2,127 @@
 #include "Encoder.h"
 
 #ifndef DEBOUNCE_DELAY
-#define DEBOUNCE_DELAY 20
+    #define DEBOUNCE_DELAY 20
 #endif
 
+Encoder::callback Encoder::onUp    = nullptr;
+Encoder::callback Encoder::onDown  = nullptr;
+// Encoder::callback Encoder::onFastUp    = nullptr;
+// Encoder::callback Encoder::onFastDown  = nullptr;
+
 // Encoder with button functionality on MUX
-Encoder::Encoder(uint8_t nExp, uint8_t pin1, uint8_t pin2, uint8_t pin3, EncPulse_t pulses)
+// Encoder::Encoder(uint8_t nExp, uint8_t pin1, uint8_t pin2, uint8_t pin3, EncPulse_t pulses)
+Encoder::Encoder(uint8_t nExp, uint8_t pin1, uint8_t pin2, EncPulse_t pulses)
 {
-  _nExp = nExp;
-  _pin1 = pin1;
-  _pin2 = pin2;
-  _pin3 = pin3;
-  _pulses = pulses;
-  _count = 0;
-  _state = 0;
-  _transition = transNone;
-  _cmdUp = -1;
-  _cmdDown = -1;
-  _cmdPush = -1;
-  pinMode(_pin1, INPUT_PULLUP);
-  pinMode(_pin2, INPUT_PULLUP);
-  if (_pin3 != NOT_USED)
-  {
-    pinMode(_pin3, INPUT_PULLUP);
-  }
+    _nExp       = nExp;
+    _pin1       = pin1;
+    _pin2       = pin2;
+    // _pin3       = pin3;
+    _pulses     = pulses;
+    _count      = 0;
+    _state      = 0;
+    // _transition = transNone;
+    _cmdUp      = -1;
+    _cmdDown    = -1;
+    // _cmdPush    = -1;
+    pinMode(_pin1, INPUT_PULLUP);
+    pinMode(_pin2, INPUT_PULLUP);
+    // if (_pin3 != NOT_USED) {
+    //     pinMode(_pin3, INPUT_PULLUP);
+    // }
 }
 
 // real time handling
 void Encoder::update()
 {
-  uint8_t newState = 0;
-  // collect new state
-  // If MCU direct pins, getBit() performs pin read, not cache read
-  // For _nExp values corresponding to I/O expanders: resort to cache read
-  // For actual multiplexers: avoid cache read, access multiplexer now
-  newState |= (DigitalIn.getBit(_nExp, _pin2, true) ? 0x02 : 0x00);
-  newState |= (DigitalIn.getBit(_nExp, _pin1, true) ? 0x01 : 0x00);
-  _state = ((_state & 0x03) << 2) | newState;
+    uint8_t newState = 0;
+    // collect new state
+    // If MCU direct pins, getBit() performs pin read, not cache read
+    // For _nExp values corresponding to I/O expanders: resort to cache read
+    // For actual multiplexers: avoid cache read, access multiplexer now
+    newState |= (DigitalIn.getBit(_nExp, _pin2, true) ? 0x02 : 0x00);
+    newState |= (DigitalIn.getBit(_nExp, _pin1, true) ? 0x01 : 0x00);
+    _state = ((_state & 0x03) << 2) | newState;
 
-  // evaluate state change
-  if (_state == 1 || _state == 7 || _state == 8 || _state == 14)
-  {
-    _count++;
-  }
-  else if (_state == 2 || _state == 4 || _state == 11 || _state == 13)
-  {
-    _count--;
-  }
-  else if (_state == 3 || _state == 12)
-  {
-    _count += 2;
-  }
-  else if (_state == 6 || _state == 9)
-  {
-    _count -= 2;
-  }
-
-  // optional button functionality
-  if (_pin3 != NOT_USED)
-  {
-    if (DigitalIn.getBit(_nExp, _pin3))
-    {
-      if (_debounce == 0)
-      {
-        _debounce = DEBOUNCE_DELAY;
-        _transition = transPressed;
-      }
+    // evaluate state change
+    if (_state == 1 || _state == 7 || _state == 8 || _state == 14) {
+        _count++;
+    } else if (_state == 2 || _state == 4 || _state == 11 || _state == 13) {
+        _count--;
+    } else if (_state == 3 || _state == 12) {
+        _count += 2;
+    } else if (_state == 6 || _state == 9) {
+        _count -= 2;
     }
-    else if (_debounce > 0)
-    {
-      if (--_debounce == 0)
-      {
-        _transition = transReleased;
-      }
-    }
-  }
+
+    // optional button functionality
+    // if (_pin3 != NOT_USED) {
+    //     if (DigitalIn.getBit(_nExp, _pin3)) {
+    //         if (_debounce == 0) {
+    //             _debounce   = DEBOUNCE_DELAY;
+    //             _transition = transPressed;
+    //         }
+    //     } else if (_debounce > 0) {
+    //         if (--_debounce == 0) {
+    //             _transition = transReleased;
+    //         }
+    //     }
+    // }
 }
 
-void Encoder::setCommand(int cmdUp, int cmdDown, int cmdPush)
-{
-  _cmdUp = cmdUp;
-  _cmdDown = cmdDown;
-  _cmdPush = cmdPush;
-}
+// void Encoder::setCommand(int cmdUp, int cmdDown) //, int cmdPush)
+// {
+//     _cmdUp   = cmdUp;
+//     _cmdDown = cmdDown;
+//     // _cmdPush = cmdPush;
+// }
 
-void Encoder::setCommand(XPString_t *cmdNameUp, XPString_t *cmdNameDown, XPString_t *cmdNamePush)
-{
-  _cmdUp = XP.registerCommand(cmdNameUp);
-  _cmdDown = XP.registerCommand(cmdNameDown);
-  _cmdPush = XP.registerCommand(cmdNamePush);
-}
+// void Encoder::setCommand(XPString_t *cmdNameUp, XPString_t *cmdNameDown, XPString_t *cmdNamePush)
+// {
+//     _cmdUp   = XP.registerCommand(cmdNameUp);
+//     _cmdDown = XP.registerCommand(cmdNameDown);
+//     // _cmdPush = XP.registerCommand(cmdNamePush);
+// }
 
 void Encoder::setCommand(int cmdUp, int cmdDown)
 {
-  _cmdUp = cmdUp;
-  _cmdDown = cmdDown;
-  _cmdPush = -1;
+    _cmdUp   = cmdUp;
+    _cmdDown = cmdDown;
+    // _cmdPush = -1;
 }
 
-void Encoder::setCommand(XPString_t *cmdNameUp, XPString_t *cmdNameDown)
-{
-  _cmdUp = XP.registerCommand(cmdNameUp);
-  _cmdDown = XP.registerCommand(cmdNameDown);
-  _cmdPush = -1;
-}
+// void Encoder::setCommand(XPString_t *cmdNameUp, XPString_t *cmdNameDown)
+// {
+//     _cmdUp   = XP.registerCommand(cmdNameUp);
+//     _cmdDown = XP.registerCommand(cmdNameDown);
+//     // _cmdPush = -1;
+// }
 
 int Encoder::getCommand(EncCmd_t cmd)
 {
-  switch (cmd)
-  {
-  case encCmdUp:
-    return _cmdUp;
-    break;
-  case encCmdDown:
-    return _cmdDown;
-    break;
-  case encCmdPush:
-    return _cmdPush;
-    break;
-  default:
+    if (cmd == encCmdUp) { return _cmdUp; }
+    else 
+    if (cmd == encCmdDown) { return _cmdDown; }
+    else 
+    // if (cmd == encCmdPush) { return _cmdPush; }
+    // else 
     return -1;
-    break;
-  }
 }
 
 void Encoder::trigger()
 {
-  if (up())
-  {
-    XP.commandTrigger(_cmdUp);
-  }
-  if (down())
-  {
-    XP.commandTrigger(_cmdDown);
-  }
-  if (_cmdPush >= 0)
-  {
-    if (pressed())
-    {
-      XP.commandStart(_cmdPush);
+    if (up()) {
+        if(onUp != nullptr) onUp(_cmdUp); // XP.commandTrigger(_cmdUp);
+    } else
+    if (down()) {
+        if(onDown != nullptr) onDown(_cmdDown); // XP.commandTrigger(_cmdDown);
     }
-    if (released())
-    {
-      XP.commandEnd(_cmdPush);
-    }
-  }
+    // if (_cmdPush >= 0) {
+    //     if (pressed()) {
+    //         XP.commandStart(_cmdPush);
+    //     }
+    //     if (released()) {
+    //         XP.commandEnd(_cmdPush);
+    //     }
+    // }
 }
